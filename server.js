89ba1271a12dcf56a75136cce270a669c7d7b283
majdkh97 <majd.motaz@yahoo.com>
2021-06-22 17:03:42 +0300

    POST REQUEST MAJD + MOHAMMAD

--- server.js
+++ server.js
@@ -4,13 +4,22 @@
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 const mongoose = require('mongoose');
-const getBooks = require('./controllers/book.controller');
+const {
+    getBooks,
+    createBooks,
+    // updateBooks,
+    // deleteBooks
+} = require('./controllers/book.controller');
 
 require('dotenv').config();
-const PORT = process.env.PORT;  
-const seedUserData = require('./models/user.model')
+const PORT = process.env.PORT;
+const {seedUserData} = require('./models/user.model')
 const cors = require('cors'); // enable the communication between the frontend and the backend
 app.use(cors())
+
+// this method is used to decode our request body sent by the post or put methods
+app.use(express.json());
+
 
 // connect to mongo db using mongoose
 mongoose.connect('mongodb://localhost:27017/myFavoriteBooks',
@@ -21,7 +30,8 @@ mongoose.connect('mongodb://localhost:27017/myFavoriteBooks',
 //we seed/ populating or filling our database with starter data
 // the seed function is responsible for populating our database with data
 // invoke/ call once
-seedUserData();
+
+// seedUserData();
 
 /*
 -------------------------------
@@ -31,12 +41,15 @@ Our routes
 
 
 app.get('/books', getBooks)
+app.post('/books', createBooks)
+// app.put('/books', updateBooks)
+// app.delete('/books', deleteBooks)
 
 
 app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-})
+    function (req, res) { // callback function of what we should do with our request
+        res.send('Hello World') // our endpoint function response
+    })
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
